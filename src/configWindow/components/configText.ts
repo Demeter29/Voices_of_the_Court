5e5485ae1e0718d5353842ee5501b3d6541c460d
Durond <gabordemeter29@gmail.com>
2024-10-02 16:07:11 +0200

    add instruct settings

---
+++ src/configWindow/components/configText.ts
@@ -0,0 +1,61 @@
+import { ipcMain, ipcRenderer } from 'electron';
+import { Config } from '../../shared/Config';
+import fs from 'fs';
+import path from 'path';
+
+const template = document.createElement("template");
+
+function defineTemplate(label: string){
+    return `
+    <link rel="stylesheet" href="../../public/configWindow/config.css">
+    <style>
+    </style>
+    <label for="awd">${label}</label><br>
+    <input type="text" name="awd" style="width: 60%">`
+    
+}
+
+    
+
+class ConfigText extends HTMLElement{
+    label: string;
+    confID: string;
+    shadow: any;
+    input: any;
+
+    constructor(){
+        super();
+        this.label = this.getAttribute("label")!;
+        this.confID = this.getAttribute("confID")!;
+
+        this.shadow = this.attachShadow({mode: "open"});
+        template.innerHTML = defineTemplate(this.label);
+        this.shadow.append(template.content.cloneNode(true));
+        this.input = this.shadow.querySelector("input");
+    }
+
+
+    static get observedAttributes(){
+        return ["name", "confID", "label"]
+    }
+
+    async connectedCallback(){
+        const confID: string = this.confID;
+
+        let config = await ipcRenderer.invoke('get-config');
+
+        //@ts-ignore
+        this.input.value = config[confID];
+
+        this.input.addEventListener("change", (e: any) => {
+            console.log(confID)
+
+            ipcRenderer.send('config-change', confID, this.input.value);
+        });
+    }
+}
+
+
+
+
+customElements.define("config-text", ConfigText);
